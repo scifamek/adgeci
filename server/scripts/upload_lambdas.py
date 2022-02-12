b5e73fb96288ebe2c1052e5710f46b4c5c7a924c
Sergio Posada Urrea <=>
2022-02-12 01:16:26 -0500

    Configuracion de stack

--- server/scripts/upload_lambdas.py
+++ server/scripts/upload_lambdas.py
@@ -6,7 +6,7 @@ execution_files = list(map(lambda x: x.replace('\\', '/'),list(glob.iglob(f'../o
 
 def upload_lambda(path):
     name = path.split('/')[2]
-    command = base_command.format(name,bucket, name)
+    command = base_command.format(f'{name}-lambda-function',bucket, name)
     print(command)
     print()
     os.system(f'aws s3 --region us-east-1 cp {path} s3://{bucket}')
