c3439682a8d09e06718da8d0d4f14cdf405e052a
Sergio Posada Urrea <=>
2022-02-12 01:03:42 -0500

    Configuracion de stack

--- server/scripts/upload_lambdas.py
+++ server/scripts/upload_lambdas.py
@@ -1,12 +1,12 @@
 import glob
 import os
 bucket = 'adgeci-lambda-zip-bucket'
-base_command = 'aws lambda update-function-code --function-name  {} --s3-bucket s3://{bucket}/{}.zip'
+base_command = 'aws lambda update-function-code --function-name  {} --s3-bucket s3://{}/{}.zip'
 execution_files = list(map(lambda x: x.replace('\\', '/'),list(glob.iglob(f'../output/**/*.zip', recursive=True))))
 
 def upload_lambda(path):
     name = path.split('/')[2]
-    command = base_command.format(name, name)
+    command = base_command.format(name,bucket, name)
     print(command)
     print()
     os.system(f'aws s3 --region us-east-1 cp {path} s3://{bucket}')
