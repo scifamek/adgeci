62244ca99f733e0b36a7bedfb675c80acc304e4e
Sergio Posada Urrea <=>
2022-02-12 01:40:48 -0500

    Configuracion de stack

--- server/scripts/upload_lambdas.py
+++ server/scripts/upload_lambdas.py
@@ -3,7 +3,6 @@ import os
 bucket = 'adgeci-lambda-zip-bucket'
 base_command = 'aws lambda update-function-code --function-name  {} --s3-bucket {}  --s3-key {}.zip'
 
-update_configuration_command = 'aws lambda update-function-configuration --function-name  {} --enviroment "Variables={MASTER_DATABASE_NAME=master,MONGODB_ATLAS_CLUSTER_URI=mongodb+srv://pochecho:sifamek666@information.ekarf.mongodb.net}"'
 execution_files = list(map(lambda x: x.replace('\\', '/'),list(glob.iglob(f'../output/**/*.zip', recursive=True))))
 
 def upload_lambda(path):
@@ -14,7 +13,7 @@ def upload_lambda(path):
     print()
     os.system(f'aws s3 --region us-east-1 cp {path} s3://{bucket}')
     os.system(command)
-    os.system(f'aws lambda update-function-configuration --function-name  { real_name } --enviroment "Variables={{MASTER_DATABASE_NAME=master,MONGODB_ATLAS_CLUSTER_URI=mongodb+srv://pochecho:sifamek666@information.ekarf.mongodb.net}}" ')
+    os.system(f'aws lambda update-function-configuration --function-name  { real_name } --environment "Variables={{MASTER_DATABASE_NAME=master,MONGODB_ATLAS_CLUSTER_URI=mongodb+srv://pochecho:sifamek666@information.ekarf.mongodb.net}}" ')
 
 for file in execution_files:
     upload_lambda(file)
