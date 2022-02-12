f64f90fbd8b826c5d97703fa2148e2f1c68f795f
Sergio Posada Urrea <=>
2022-02-12 01:38:44 -0500

    Configuracion de stack

--- server/scripts/upload_lambdas.py
+++ server/scripts/upload_lambdas.py
@@ -8,12 +8,13 @@ execution_files = list(map(lambda x: x.replace('\\', '/'),list(glob.iglob(f'../o
 
 def upload_lambda(path):
     name = path.split('/')[2]
-    command = base_command.format(f'{name}-lambda-function',bucket, name)
+    real_name = f'{name}-lambda-function'
+    command = base_command.format(f'{real_name}',bucket, name)
     print(command)
     print()
     os.system(f'aws s3 --region us-east-1 cp {path} s3://{bucket}')
     os.system(command)
-    os.system(update_configuration_command.format(f'{name}-lambda-function'))
+    os.system(f'aws lambda update-function-configuration --function-name  { real_name } --enviroment "Variables={{MASTER_DATABASE_NAME=master,MONGODB_ATLAS_CLUSTER_URI=mongodb+srv://pochecho:sifamek666@information.ekarf.mongodb.net}}" ')
 
 for file in execution_files:
     upload_lambda(file)
